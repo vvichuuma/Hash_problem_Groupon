1e534e234389b57f1ccd696b39c4da36b81be7fb
VISHNU <vvichugit@gmail.com>
2019-06-21 13:59:18 -0500

    added the sort Function

---
+++ sortArray.js
@@ -0,0 +1,19 @@
+
+
+//Sorting an array in JAVASCRIPT : 
+
+
+function array(ar){
+
+  var ascending = ar.sort(function(a,b) {return(a-b)})
+  var descending  = ar.sort(function(a,b) {return (b-a)})
+
+  console.log(ascending); 
+  console.log(descending);
+
+  //If you console log - It is gonna have the same output because , IT is changing the value - which is refrencing the array ar.
+
+}
+
+
+array([2,3,4,10,12,14])
